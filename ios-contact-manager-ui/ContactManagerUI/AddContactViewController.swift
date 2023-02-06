6d37c30612997d3b1afa1d72588e851be3561044
SunnnySong <tjsghktjswls@naver.com>
2023-02-06 23:50:15 +0900

    feat: 연락처 입력 hyphen 구현

--- ios-contact-manager-ui/ContactManagerUI/AddContactViewController.swift
+++ ios-contact-manager-ui/ContactManagerUI/AddContactViewController.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 final class AddContactViewController: UIViewController {
-
+    
     @IBOutlet weak var contactTextField: UITextField!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         contactTextField.delegate = self
@@ -19,6 +19,36 @@ final class AddContactViewController: UIViewController {
 
 extension AddContactViewController: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if let text = textField.text, let textRange = Range(range, in: text) {
+            let updatedText = text.replacingCharacters(in: textRange, with: string)
+            
+            if text.count == 2 && updatedText.count == 3 {
+                textField.text = text + "-" + string
+                return false
+            }
+            if text.count == 6 && updatedText.count == 7 {
+                textField.text = text + "-" + string
+                return false
+            }
+            
+            if text.count == 11 && updatedText.count == 12 {
+                var normalText = text.replacingOccurrences(of: "-", with: "")
+                normalText.insert("-", at: normalText.index(normalText.startIndex, offsetBy: 3))
+                normalText.insert("-", at: normalText.index(normalText.startIndex, offsetBy: 7))
+                textField.text = normalText
+            }
+            
+            if text.count == 12 && updatedText.count == 13 {
+                var normalText = text.replacingOccurrences(of: "-", with: "")
+                normalText.insert("-", at: normalText.index(normalText.startIndex, offsetBy: 3))
+                normalText.insert("-", at: normalText.index(normalText.startIndex, offsetBy: 8))
+                textField.text = normalText
+            }
+            
+        }
         return true
     }
 }
+
+
